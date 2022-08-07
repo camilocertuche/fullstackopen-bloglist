95f232348c81d43603466d620ff8f48b73e7746c
Camilo <camilo.certuche@gmail.com>
2022-08-06 23:21:38 -0500

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) return null;
+
+  const likesList = blogs.map((blog) => blog.likes);
+  const max = Math.max(...likesList);
+
+  const favorite = blogs.find((blog) => blog.likes === max);
+  const { title, author, likes } = favorite;
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
