06145a4b1ca7eda245dcfab48b6e05e58c7a8932
Camilo <camilo.certuche@gmail.com>
2022-08-15 12:58:45 -0500

    Implement middleware

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const blogRouter = require("./controllers/blogs");
-const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+require("express-async-errors");
+
+const blogRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -17,5 +20,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
