7ab99515887f3b7484f78e21107f1984571d243a
Camilo <camilo.certuche@gmail.com>
2022-08-15 21:20:59 -0500

    4.17: bloglist expansion, step5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   return response.json(blogs);
 });
 
@@ -14,7 +15,15 @@ blogRouter.post("/", async (request, response) => {
     return response.status(400).json({ error: "title and url are required" });
   }
 
+  const users = await User.find({});
+  const user = users[0];
+
+  blog.user = user._id;
+
   const result = await blog.save();
+
+  user.blogs = user.blogs.concat(result._id);
+  await user.save();
 
   response.status(201).json(result);
 });
--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,11 @@ userRouter.post("/", async (request, response) => {
 });
 
 userRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+  });
   response.json(users.map((user) => user.toJSON()));
 });
 
